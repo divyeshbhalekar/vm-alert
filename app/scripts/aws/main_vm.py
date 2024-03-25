41d3539b8ea6fe0e3be5372b67901ea2dd935465
Divyesh3011 <divyesh@microsec.ai>
2024-03-25 12:53:40 +0530

    added only 2 region

--- app/scripts/aws/main_vm.py
+++ app/scripts/aws/main_vm.py
@@ -48,11 +48,7 @@ def main(aws_access_key_id, aws_secret_access_key, slack_token, slack_channel):
     slack_client = WebClient(token=slack_token)
 
     aws_regions = [
-        'us-east-1', 'us-east-2' ,'us-west-1', 'us-west-2',
-        'ap-south-1', 'ap-southeast-1',
-        'ap-southeast-2','ap-northeast-3', 'ap-northeast-2',
-        'ap-northeast-1', 'ca-central-1', 'eu-north-1', 'eu-west-3',
-        'eu-west-2', 'eu-west-1', 'eu-south-1', 'eu-central-1'
+        'us-east-1', 'ap-south-1'
     ]  # Add more regions as needed
 
     for region in aws_regions:
